4ff7eb640a3c7f7b875d0c8ee8a0b36bf7914c46
bubu99947 <nestor.gga27@gmail.com>
2017-05-26 22:48:39 -0500

    parte exterior del proyecto

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE TABLE [dbo].Membresias(
+	ID int not null primary key identity, 
+	Nombre varchar(50) not null,
+	Descripcion varchar(150) not null, 
+	Precio money not null, 
+	Duracion integer not null, 
+);
+	
